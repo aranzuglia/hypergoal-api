6c6facac43c6f3d4e3eec50ac328f5c2eba14017
Atilio Ranzuglia <atilio.ranzuglia@9spokes.com>
2017-12-05 21:03:02 +1300

    Used JSON.stringify

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 exports.root = function rootApi (req, res) {
-  res.status(200).send('{"a":"b"}');
+  var result =
+    {
+      "_links" : {
+        "self" : {
+          "href" : "https://us-central1-hypergoal-186807.cloudfunctions.net/root",
+          "templated" : false
+        }
+      }
+    };
+
+  res.status(200).send(JSON.stringify(result));
 };
